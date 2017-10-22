60b888497e65793dbb2529cb1d14dc7d863e2b6f
Jerin John <jerin.j@somaiya.edu>
2017-10-23 02:04:56 +0530

    Media Stream Trial Configured.

---
+++ app/src/main/java/com/anthrino/raspicontroller/mediaObject.java
@@ -0,0 +1,12 @@
+package com.anthrino.raspicontroller;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Johns on 10/23/2017.
+ */
+
+public class mediaObject {
+
+    static Bitmap photo = null;
+}
