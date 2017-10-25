ae6365ce37dfa2631a0f6fc4022a302030aa7249
Mit <mit.jain@somaiya.edu>
2017-10-25 12:10:18 +0530

    ProgressBar Stable build

--- app/src/main/java/com/anthrino/raspicontroller/videoPlayback.java
+++ app/src/main/java/com/anthrino/raspicontroller/videoPlayback.java
@@ -3,8 +3,12 @@ package com.anthrino.raspicontroller;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.FloatRange;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -20,6 +24,7 @@ public class videoPlayback extends AppCompatActivity {
     public static TextView title;
     private Uri media;
     public static ProgressBar videoDownloadProgressBar;
+    public static LinearLayout frameLayout;
 
     @Override
 
@@ -27,6 +32,7 @@ public class videoPlayback extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.video_playback);
         videoView = (VideoView) findViewById(R.id.fragmentVideo);
+        frameLayout = (LinearLayout) findViewById(R.id.videoLayout);
         title = (TextView) findViewById(R.id.videoTitle);
         title.setText(getIntent().getStringExtra("filename"));
         videoDownloadProgressBar = (ProgressBar) findViewById(R.id.videoDownloadProgressbar);
@@ -41,8 +47,9 @@ public class videoPlayback extends AppCompatActivity {
         videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
 
             public void onPrepared(MediaPlayer mp) {
-                videoView.start();
                 videoDownloadProgressBar.setVisibility(View.GONE);
+                frameLayout.setVisibility(View.VISIBLE);
+                videoView.start();
 
             }
         });
